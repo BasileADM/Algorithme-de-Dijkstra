7612626f3afd24b193ab956f03db5fb2229cda78
Basile ADM <aychikhi07@gmail.com>
2025-04-24 11:31:53 +0200

    commit 1

---
+++ src/dijkstra/Dijkstra.java
@@ -0,0 +1,10 @@
+package dijkstra;
+
+import graph.Graph;
+import graph.ShortestPath;
+
+public class Dijkstra<S> {
+    public ShortestPath.Distances<S> compute(Graph g, S from) {
+        return null;
+    }
+}
