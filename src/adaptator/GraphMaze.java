72df264c1df9e78173fc46cef3ad31cbdf991985
Basile.Clmnt <120327869+BasileADM@users.noreply.github.com>
2025-05-18 15:15:23 +0200

    dernière vérification et débogage

--- src/adaptator/GraphMaze.java
+++ src/adaptator/GraphMaze.java
@@ -9,19 +9,18 @@ import java.util.Set;
 
 public class GraphMaze<T> implements Graph<T> {
 
-    private final Maze<T> maze;
-
+    private Maze<T> maze;
     public GraphMaze(Maze<T> maze) {
         this.maze = maze;
     }
 
-
     @Override
     public List<Arc<T>> getSucc(T s) {
         Set<T> voisins = maze.openedNeighbours(s);
-        List<Arc<T>> arcs = new ArrayList<>();
-        for (T voisin : voisins)
-            arcs.add(new Arc<>(1, voisin)); // Chaque arc a un poids de 1 (si pas spécifié)
-        return arcs;
+        List<Arc<T>> succs = new ArrayList<>();
+        for (T v : voisins) {
+            succs.add(new Arc<>(1, v));//chaque arc est par default à un poids de 1
+        }
+        return succs;
     }
 }
